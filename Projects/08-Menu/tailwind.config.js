c68865c8ae3c90f62dd8f0b15232c91c0ad071c4
James-alderson <james.alderson.a@gmail.com>
2022-10-27 18:59:48 +0330

    Add files
    - Add favicon.
    - Add 'menu' images.
    - Create 'input.css'.
    - Create 'data.json'.
    - Define 'package.json'.
    - install 'tailwindcss'.

---
+++ Projects/08-Menu/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screen: {
+      sm: "36rem", // 576px
+      md: "48rem", // 768px
+      lg: "62rem", // 992px
+      xl: "75rem", // 1200px
+      xxl: "87.5rem", // 1400px
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
